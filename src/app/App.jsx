79ff9c993b47497ebc4183fa881aba40c616d979
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-05 02:56:16 +0300

    [04-events-trigger] 1. save input value without "onChange"

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class App extends Component {
+    addTrack() {
+        console.log('add track =>', this.trackInput.value);
+    }
+
     render() {
         console.log('testStore =>', this.props.testStore);
 
         return (
             <div className="app">
-                <input type="text" />
-                <button>Add track</button>
+                <input
+                    type="text"
+                    ref={input => {
+                        this.trackInput = input;
+                    }}
+                />
+
+                <button onClick={this.addTrack.bind(this)}>Add track</button>
+
                 <ul>
                     {this.props.testStore.map((track, index) => {
                         return <li key={index}>{track}</li>;
