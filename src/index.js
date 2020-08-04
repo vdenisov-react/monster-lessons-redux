2ae30c741ab06715994efc058f7b95c4277efe52
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-05 02:01:14 +0300

    [03-redux-connect] 2. use Provider for sharing store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-
-// import * as serviceWorker from './utils/serviceWorker';
-// import './index.css';
-
-// import App from './app/App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-// serviceWorker.unregister();
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+
+import * as serviceWorker from './utils/serviceWorker';
+import './index.css';
+
+import App from './app/App';
 
 function playlist(state = [], action) {
     console.log('action =>', action);
@@ -22,22 +18,43 @@ function playlist(state = [], action) {
 
 const store = createStore(playlist);
 
-const addTrackBtn = document.querySelectorAll('.addTrack')[0];
-const trackInput = document.querySelectorAll('.trackInput')[0];
-const list = document.querySelectorAll('.list')[0];
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
 
-store.subscribe(() => {
-    const arr = store.getState();
-    list.innerHTML = '';
-    arr.forEach(track => {
-        const li = document.createElement('li');
-        li.textContent = track;
-        list.appendChild(li);
-    });
-});
+serviceWorker.unregister();
 
-addTrackBtn.addEventListener('click', () => {
-    const trackName = trackInput.value;
-    store.dispatch({ type: 'ADD_TRACK', payload: trackName });
-    trackInput.value = '';
-});
+// import { createStore } from 'redux';
+
+// function playlist(state = [], action) {
+//     console.log('action =>', action);
+//     if (action.type === 'ADD_TRACK') {
+//         return [...state, action.payload];
+//     }
+//     return state;
+// }
+
+// const store = createStore(playlist);
+
+// const addTrackBtn = document.querySelectorAll('.addTrack')[0];
+// const trackInput = document.querySelectorAll('.trackInput')[0];
+// const list = document.querySelectorAll('.list')[0];
+
+// store.subscribe(() => {
+//     const arr = store.getState();
+//     list.innerHTML = '';
+//     arr.forEach(track => {
+//         const li = document.createElement('li');
+//         li.textContent = track;
+//         list.appendChild(li);
+//     });
+// });
+
+// addTrackBtn.addEventListener('click', () => {
+//     const trackName = trackInput.value;
+//     store.dispatch({ type: 'ADD_TRACK', payload: trackName });
+//     trackInput.value = '';
+// });
