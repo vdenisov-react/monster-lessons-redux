a0dc242bb760d07256215e2c765e6119227d524a
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-05 00:03:50 +0300

    [01-redux-intro] 3. handle action and update store

--- src/index.js
+++ src/index.js
@@ -12,10 +12,19 @@
 
 import { createStore } from 'redux';
 
-function playlist(state = []) {
+function playlist(state = [], action) {
+    console.log('action =>', action);
+    if (action.type === 'ADD_TRACK') {
+        return [...state, action.payload];
+    }
     return state;
 }
 
 const store = createStore(playlist);
 
-console.log('store =>', store.getState());
+store.subscribe(() => {
+    console.log('subscribe =>', store.getState());
+});
+
+store.dispatch({ type: 'ADD_TRACK', payload: 'Smells like spirit' });
+store.dispatch({ type: 'ADD_TRACK', payload: 'Enter Sandman' });
