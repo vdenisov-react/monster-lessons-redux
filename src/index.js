8256929caa3e054b89b803b2f40e4c1acad1ce47
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-04 23:12:12 +0300

    remove using "Strict Mode" for app

--- src/index.js
+++ src/index.js
@@ -6,12 +6,6 @@ import './index.css';
 
 import App from './app/App';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-
-    document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
