ef469eeedea0266257a937f930ebff1ddf6c04b9
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-05 00:03:07 +0300

    [01-redux-intro] 2. init simple store (+ get value)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import * as serviceWorker from './utils/serviceWorker';
-import './index.css';
+// import * as serviceWorker from './utils/serviceWorker';
+// import './index.css';
 
-import App from './app/App';
+// import App from './app/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.unregister();
+// serviceWorker.unregister();
+
+import { createStore } from 'redux';
+
+function playlist(state = []) {
+    return state;
+}
+
+const store = createStore(playlist);
+
+console.log('store =>', store.getState());
