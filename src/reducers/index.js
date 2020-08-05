9d4d5ce04d65af7a32d3b06fba26194eda36d896
Vladimir Denisov <vdenisov.dev@gmail.com>
2020-08-05 16:06:52 +0300

    [09-redux-router] 2. combine router reducer with others

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 
 import tracksReducer from './tracks.reducer';
 import playlistsReducer from './playlists.reducer';
 import filterTracksReducer from './filter-tracks.reducer';
 
 export default combineReducers({
+    routing: routerReducer,
+    // ---
     tracks: tracksReducer,
     playlists: playlistsReducer,
     filterTracks: filterTracksReducer,
